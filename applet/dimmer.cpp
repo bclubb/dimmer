05897c8618bc7bcba16f6b988d13d33420124de9
Brian Clubb <bclubb@gmail.com>
2009-06-02 23:58:23 -0500

    it's snoring

--- applet/dimmer.cpp
+++ applet/dimmer.cpp
@@ -1,6 +1,9 @@
 // Fade the LED like it is snoring
 
 #define LED 9
+#include "WProgram.h"
+void setup();
+void loop();
 int i = 0;
 
 void setup() {
@@ -14,9 +17,21 @@ void loop() {
     delay(10);
   }
   
-  for(i = 255, i > 0, i--)
+  for(i = 255; i > 0; i--)
   {
     analogWrite(LED, i);
     delay(10);
   }
 }
+int main(void)
+{
+	init();
+
+	setup();
+    
+	for (;;)
+		loop();
+        
+	return 0;
+}
+
